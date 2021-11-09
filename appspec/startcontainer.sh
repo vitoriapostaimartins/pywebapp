307b5714f7f54662d1179328335026ad1c4d1fc1
Vitoria Postai Martins <49215543+vitoriapostaimartins@users.noreply.github.com>
2021-11-09 12:14:43 -0800

    Update this file with my ECS details

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 170015466808.dkr.ecr.us-west-2.amazonaws.com
-docker pull 170015466808.dkr.ecr.us-west-2.amazonaws.com/farnazdargahi/pywebapp:latest || {
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 039625559182.dkr.ecr.us-west-2.amazonaws.com
+docker pull 039625559182.dkr.ecr.us-west-2.amazonaws.com/vitoriapostaimartins/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
 }
-docker run --name pywebapp -p 80:8080 --detach 170015466808.dkr.ecr.us-west-2.amazonaws.com/farnazdargahi/pywebapp:latest
+docker run --name pywebapp -p 80:8080 --detach 039625559182.dkr.ecr.us-west-2.amazonaws.com/vitoriapostaimartins/pywebapp:latest
